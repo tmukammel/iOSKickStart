6e9c5b3cb127c27f71ff3a9251271b52b8481b7d
Twaha Mukammel <tmukammel@Noors-MacBook-Pro.local>
2017-08-10 04:29:58 +0600

    Example scrollView

---
+++ Example/iOSKickStart/TestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TestViewController.swift
+//  iOSKickStart
+//
+//  Created by Twaha Mukammel on 8/10/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
